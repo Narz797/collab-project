c93032795d77b352c09e30e950e152321ebe1174
Joshua Bayron <bayronjoshua333@gmail.com>
2024-05-22 21:19:11 +0800

    initial commint

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, Routes } from 'react-router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    
+    <Route>
+      <Routes>
+        
+      </Routes>
+    </Route>
   </React.StrictMode>,
 )
