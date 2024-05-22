9fe6c318f75223cd854d2e7701344d696ff6b30b
Joshua Bayron <bayronjoshua333@gmail.com>
2024-05-22 21:37:16 +0800

    added home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Route, Routes } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import {Home} from './assets/pages/home';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Route>
+    <BrowserRouter>
       <Routes>
-        
+        <Route path="/" element={<Home />}>
+          <Route path="/Home" element={<Home />}></Route>
+        </Route>
       </Routes>
-    </Route>
+    </BrowserRouter>
   </React.StrictMode>,
 )
